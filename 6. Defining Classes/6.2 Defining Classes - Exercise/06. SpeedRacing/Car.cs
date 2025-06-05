17c8c8a0b80ee43fa77995b9d92d88509b2d1697
ASYA <asyailieva@yahoo.com>
2025-06-05 11:46:46 +0100

    Add solution to Defining Classes - Exercise
    Add solution to Defining Classes - Exercise

---
+++ 6. Defining Classes/6.2 Defining Classes - Exercise/06. SpeedRacing/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumption;
+        private double travelledDistance;
+
+        public string Model
+        {
+            get => this.model;
+            set => this.model = value;
+        }
+
+        public double FuelAmount
+        {
+            get => this.fuelAmount;
+            set => this.fuelAmount = value;
+        }
+
+        public double FuelConsumption
+        {
+            get => this.fuelConsumption;
+            set => this.fuelConsumption = value;
+        }
+
+        public double TravelledDistance
+        {
+            get => this.travelledDistance;
+            set => this.travelledDistance = value;
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumption) 
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumption = fuelConsumption;        
+        }
+
+        public bool Drive(double distance)
+        {
+            double neededFuel = distance * this.FuelConsumption;
+            if(this.FuelAmount < neededFuel) return false;
+
+            this.FuelAmount -= neededFuel;
+            this.TravelledDistance += distance;
+            return true;            
+        }
+    }
+}
---
+++ 6. Defining Classes/6.2 Defining Classes - Exercise/06. SpeedRacing/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SpeedRacing;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, Car> cars = new Dictionary<string, Car>();
+
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string model = data[0];
+            double fuelAmount = double.Parse(data[1]), fuelConsumption = double.Parse(data[2]);
+
+            Car car = new Car(model, fuelAmount, fuelConsumption);
+            cars[car.Model] = car;
+        }
+
+        string command;
+        while ((command = Console.ReadLine()) != "End")
+        {
+            string[] data = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string model = data[1];
+            double distance = double.Parse(data[2]);
+
+            Car car = cars[model];
+            if(!car.Drive(distance))
+                Console.WriteLine("Insufficient fuel for the drive"); 
+        }
+
+        foreach(Car car in cars.Values)
+            Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.TravelledDistance}");
+
+
+    }
+}
